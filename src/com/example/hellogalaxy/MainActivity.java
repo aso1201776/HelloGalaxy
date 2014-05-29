d20ed1a06fe8f48bcf6c8d5e62b07b38b1c6027e
student <student@PC111S10.fgakusei.local>
2014-05-29 14:16:48 +0900

    HelloGalaxy

---
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hellogalaxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		// ボタンをIDでさがしてボタン変数をつくる
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()) {
+			case R.id.btnOK:
+				// エディットテキストから入力内容を取り出す
+				EditText etv = (EditText)findViewById(R.id.edtFirst);
+				String inputMsg = etv.getText().toString();
+				EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+				String inputMsg2 = etv2.getText().toString();
+
+				// メッセージ表示用のTextViewを探して、文字を設定
+				TextView tv = (TextView)findViewById(R.id.txvMsg);
+				tv.setText("あなたでしたか、" + inputMsg + inputMsg2 + "さん。");
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
